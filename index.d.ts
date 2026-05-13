4d385a17d2ac36b34cdec3bf473b47be53ade9a9
thabitmohammed300-hash <thabitmohammed300@gmail.com>
2026-05-13 22:44:12 +0200

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,179 +1,124 @@
-/**
- * An events map is an interface that maps event names to their value, which
- * represents the type of the `on` listener.
- */
-export interface EventsMap {
-    [event: string]: any;
+import type { WatchOptions } from 'chokidar';
+
+export type NodemonEventHandler =
+  | 'start'
+  | 'crash'
+  | 'exit'
+  | 'quit'
+  | 'restart'
+  | 'config:update'
+  | 'log'
+  | 'readable'
+  | 'stdout'
+  | 'stderr';
+
+export type NodemonEventListener = {
+  on(event: 'start' | 'crash' | 'readable', listener: () => void): Nodemon;
+  on(event: 'log', listener: (e: NodemonEventLog) => void): Nodemon;
+  on(event: 'stdout' | 'stderr', listener: (e: string) => void): Nodemon;
+  on(event: 'restart', listener: (files?: string[]) => void): Nodemon;
+  on(event: 'quit', listener: (e?: NodemonEventQuit) => void): Nodemon;
+  on(event: 'exit', listener: (e?: number) => void): Nodemon;
+  on(
+    event: 'config:update',
+    listener: (e?: NodemonEventConfig) => void,
+  ): Nodemon;
+};
+
+export type NodemonEventLog = {
+  /**
+    - detail: what you get with nodemon --verbose.
+    - status: subprocess starting, restarting.
+    - fail: is the subprocess crashing.
+    - error: is a nodemon system error.
+  */
+  type: 'detail' | 'log' | 'status' | 'error' | 'fail';
+  /** the plain text message */
+  message: string;
+  /** contains the terminal escape codes to add colour, plus the "[nodemon]" prefix */
+  colour: string;
+};
+
+export type NodemonEventQuit = 143 | 130;
+
+export type NodemonEventConfig = {
+  run: boolean;
+  system: {
+    cwd: string;
+  };
+  required: boolean;
+  dirs: string[];
+  timeout: number;
+  options: NodemonConfig;
+  lastStarted: number;
+  loaded: string[];
+  load: (
+    settings: NodemonSettings,
+    ready: (config: NodemonEventConfig) => void,
+  ) => void;
+  reset: () => void;
+};
+
+export interface NodemonExecOptions {
+  script: string;
+  scriptPosition?: number;
+  args?: string[];
+  ext?: string; // "js,mjs" etc (should really support an array of strings, but I don't think it does right now)
+  exec?: string; // node, python, etc
+  execArgs?: string[]; // args passed to node, etc,
+  nodeArgs?: string[]; // args passed to node, etc,
 }
 
-/**
- * The default events map, used if no EventsMap is given. Using this EventsMap
- * is equivalent to accepting all event names, and any data.
- */
-export interface DefaultEventsMap {
-    [event: string]: (...args: any[]) => void;
+export interface NodemonConfig {
+  /** restartable defaults to "rs" as a string the user enters */
+  restartable?: false | string;
+  colours?: boolean;
+  execMap?: { [key: string]: string };
+  ignoreRoot?: string[];
+  watch?: string[];
+  ignore?: string[];
+  stdin?: boolean;
+  runOnChangeOnly?: boolean;
+  verbose?: boolean;
+  signal?: string;
+  stdout?: boolean;
+  watchOptions?: WatchOptions;
+  help?: string;
+  version?: boolean;
+  cwd?: string;
+  dump?: boolean;
+  delay?: number;
+  monitor?: string[];
+  spawn?: boolean;
+  noUpdateNotifier?: boolean;
+  legacyWatch?: boolean;
+  pollingInterval?: number;
+  /** @deprecated as this is "on" by default */
+  js?: boolean;
+  quiet?: boolean;
+  configFile?: string;
+  exitCrash?: boolean;
+  execOptions?: NodemonExecOptions;
 }
 
-/**
- * Returns a union type containing all the keys of an event map.
- */
-export type EventNames<Map extends EventsMap> = keyof Map & (string | symbol);
-
-/** The tuple type representing the parameters of an event listener */
-export type EventParams<
-    Map extends EventsMap,
-    Ev extends EventNames<Map>
-    > = Parameters<Map[Ev]>;
-
-/**
- * The event names that are either in ReservedEvents or in UserEvents
- */
-export type ReservedOrUserEventNames<
-    ReservedEventsMap extends EventsMap,
-    UserEvents extends EventsMap
-    > = EventNames<ReservedEventsMap> | EventNames<UserEvents>;
-
-/**
- * Type of a listener of a user event or a reserved event. If `Ev` is in
- * `ReservedEvents`, the reserved event listener is returned.
- */
-export type ReservedOrUserListener<
-    ReservedEvents extends EventsMap,
-    UserEvents extends EventsMap,
-    Ev extends ReservedOrUserEventNames<ReservedEvents, UserEvents>
-    > = FallbackToUntypedListener<
-    Ev extends EventNames<ReservedEvents>
-        ? ReservedEvents[Ev]
-        : Ev extends EventNames<UserEvents>
-        ? UserEvents[Ev]
-        : never
-    >;
-
-/**
- * Returns an untyped listener type if `T` is `never`; otherwise, returns `T`.
- *
- * This is a hack to mitigate https://github.com/socketio/socket.io/issues/3833.
- * Needed because of https://github.com/microsoft/TypeScript/issues/41778
- */
-type FallbackToUntypedListener<T> = [T] extends [never]
-    ? (...args: any[]) => void | Promise<void>
-    : T;
-
-/**
- * Strictly typed version of an `EventEmitter`. A `TypedEventEmitter` takes type
- * parameters for mappings of event names to event data types, and strictly
- * types method calls to the `EventEmitter` according to these event maps.
- *
- * @typeParam ListenEvents - `EventsMap` of user-defined events that can be
- * listened to with `on` or `once`
- * @typeParam EmitEvents - `EventsMap` of user-defined events that can be
- * emitted with `emit`
- * @typeParam ReservedEvents - `EventsMap` of reserved events, that can be
- * emitted by socket.io with `emitReserved`, and can be listened to with
- * `listen`.
- */
-export class Emitter<
-    ListenEvents extends EventsMap,
-    EmitEvents extends EventsMap,
-    ReservedEvents extends EventsMap = {}
-    > {
-    /**
-     * Adds the `listener` function as an event listener for `ev`.
-     *
-     * @param ev Name of the event
-     * @param listener Callback function
-     */
-    on<Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>>(
-        ev: Ev,
-        listener: ReservedOrUserListener<ReservedEvents, ListenEvents, Ev>
-    ): this;
-
-    /**
-     * Adds a one-time `listener` function as an event listener for `ev`.
-     *
-     * @param ev Name of the event
-     * @param listener Callback function
-     */
-    once<Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>>(
-        ev: Ev,
-        listener: ReservedOrUserListener<ReservedEvents, ListenEvents, Ev>
-    ): this;
-
-    /**
-     * Removes the `listener` function as an event listener for `ev`.
-     *
-     * @param ev Name of the event
-     * @param listener Callback function
-     */
-    off<Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>>(
-        ev?: Ev,
-        listener?: ReservedOrUserListener<ReservedEvents, ListenEvents, Ev>
-    ): this;
-
-    /**
-     * Emits an event.
-     *
-     * @param ev Name of the event
-     * @param args Values to send to listeners of this event
-     */
-    emit<Ev extends EventNames<EmitEvents>>(
-        ev: Ev,
-        ...args: EventParams<EmitEvents, Ev>
-    ): this;
-
-    /**
-     * Emits a reserved event.
-     *
-     * This method is `protected`, so that only a class extending
-     * `StrictEventEmitter` can emit its own reserved events.
-     *
-     * @param ev Reserved event name
-     * @param args Arguments to emit along with the event
-     */
-    protected emitReserved<Ev extends EventNames<ReservedEvents>>(
-        ev: Ev,
-        ...args: EventParams<ReservedEvents, Ev>
-    ): this;
-
-    /**
-     * Returns the listeners listening to an event.
-     *
-     * @param event Event name
-     * @returns Array of listeners subscribed to `event`
-     */
-    listeners<Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>>(
-        event: Ev
-    ): ReservedOrUserListener<ReservedEvents, ListenEvents, Ev>[];
-
-    /**
-     * Returns true if there is a listener for this event.
-     *
-     * @param event Event name
-     * @returns boolean
-     */
-    hasListeners<
-        Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>
-        >(event: Ev): boolean;
-
-    /**
-     * Removes the `listener` function as an event listener for `ev`.
-     *
-     * @param ev Name of the event
-     * @param listener Callback function
-     */
-    removeListener<
-        Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>
-        >(
-        ev?: Ev,
-        listener?: ReservedOrUserListener<ReservedEvents, ListenEvents, Ev>
-    ): this;
-
-    /**
-     * Removes all `listener` function as an event listener for `ev`.
-     *
-     * @param ev Name of the event
-     */
-    removeAllListeners<
-        Ev extends ReservedOrUserEventNames<ReservedEvents, ListenEvents>
-        >(ev?: Ev): this;
+export interface NodemonSettings extends NodemonConfig, NodemonExecOptions {
+  events?: Record<string, string>;
+  env?: Record<string, string>;
 }
+
+export type Nodemon = {
+  (settings: NodemonSettings): Nodemon;
+  removeAllListeners(event: NodemonEventHandler): Nodemon;
+  emit(type: NodemonEventHandler, event?: any): Nodemon;
+  reset(callback: Function): Nodemon;
+  restart(): Nodemon;
+  config: NodemonSettings;
+} & NodemonEventListener & {
+    [K in keyof NodemonEventListener as 'addListener']: NodemonEventListener[K];
+  } & {
+    [K in keyof NodemonEventListener as 'once']: NodemonEventListener[K];
+  };
+
+declare const nodemon: Nodemon;
+
+export = nodemon;
